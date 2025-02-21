aea595e3574cd0b5cdc425cff1f007e13cc169ff
LAPTOP-15B32R05\Umar <umarinayat111@gmail.com>
2025-02-22 04:16:26 +0500

    First Commit

---
+++ POC/POC.Client/Models/Animal.cs
@@ -0,0 +1,9 @@
+﻿namespace POC.Client.Models;
+
+public class Animal
+{
+    public string Name { get; set; } = string.Empty;
+    public string Species { get; set; } = string.Empty;
+    public bool IsVaccinated { get; set; }
+    public DateTime? VaccinationDate { get; set; }
+}
